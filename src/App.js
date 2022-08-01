b8803590a4fb80f74cba75f1cea905153caa9feb
Filipe Leoncio <filipealeoncio@gmail.com>
2022-08-01 01:42:44 -0300

    feat: Adicionado rota e componente inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PATH_NAMES } from './constants/pathNames';
 import './App.css';
+import { Home } from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React111
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={PATH_NAMES.INICIO} element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
