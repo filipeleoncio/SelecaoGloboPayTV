716a7a5bd86f748f505836fcf798b14f98429df3
Filipe Leoncio <filipealeoncio@gmail.com>
2022-08-01 22:57:32 -0300

    refactor: Ajustado arquivos iniciais da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PATH_NAMES } from './constants/pathNames';
-import './App.css';
 import { Home } from './pages/Home';
 
 function App() {
