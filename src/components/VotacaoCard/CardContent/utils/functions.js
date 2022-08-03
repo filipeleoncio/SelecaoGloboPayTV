ba8683a9f940a1495027a158855678d615fec540
Filipe Leoncio <filipealeoncio@gmail.com>
2022-08-02 21:59:02 -0300

    feat: Adicionado constantes de texto para o card de votação

---
+++ src/components/VotacaoCard/CardContent/utils/functions.js
@@ -0,0 +1,7 @@
+import { VOTACAO_TEXT } from '../../../../constants/votacaoText';
+
+export function getDescricaoParticipante(index, nome, sexo) {
+    return index === 1
+        ? VOTACAO_TEXT.descricaoParticipante1(nome, sexo)
+        : VOTACAO_TEXT.descricaoParticipante2(nome, sexo);
+}
---
+++ src/constants/votacaoText.js
@@ -0,0 +1,19 @@
+export const VOTACAO_TEXT = {
+    titulo: (
+        <>
+            QUEM DEVE SER <b>ELIMINADO</b>?
+        </>
+    ),
+    descricaoParticipante1: (nome, sexo) => (
+        <>
+            Para eliminar {sexo === 'm' ? 'o' : 'a'} <b>{nome}</b> pelo telefone disque <b>0800-123-001</b> ou mande um
+            SMS para <b>8001</b>
+        </>
+    ),
+    descricaoParticipante2: (nome, sexo) => (
+        <>
+            Para eliminar {sexo === 'm' ? 'o' : 'a'} <b>{nome}</b> pelo telefone disque <b>0800-123-002</b> ou mande um
+            SMS para <b>8002</b>
+        </>
+    ),
+};
