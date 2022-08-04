2d235b7d2f1c97cc3ef1d53df2d820e1cfeaa28e
Filipe Leoncio <filipealeoncio@gmail.com>
2022-08-04 01:28:09 -0300

    feat: Adicionado store provider no arquivo raiz do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PATH_NAMES } from './constants/pathNames';
 import { Home } from './pages/Home';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={PATH_NAMES.INICIO} element={<Home />} />
-            </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={PATH_NAMES.INICIO} element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
