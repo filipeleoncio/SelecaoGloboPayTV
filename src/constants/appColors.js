0727cd6ee0f21a265105eb051d4b06576fa0ef82
Filipe Leoncio <filipealeoncio@gmail.com>
2022-08-05 21:54:36 -0300

    feat: Adicionado cores as constantes

--- src/constants/appColors.js
+++ src/constants/appColors.js
@@ -1,8 +1,10 @@
 export const APP_COLORS = {
     primaryGrey: '#d6d6d6',
     secondaryGrey: '#f3f3f3',
+    terciaryGrey: '#5a5a5a',
     textGrey: '#5c5c5c',
-    primaryOrange: 'orange',
+    secondaryTextGrey: '#a4a4a4',
+    primaryOrange: '#ff9516',
     primaryButtonBlue: '#259afe',
     secondaryButtonBlue: '#116ebf',
     borderButtonBlue: '#0b8cf3',
