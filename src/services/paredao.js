5d3c3871fd5148399a11f035ddcbd6958ea608eb
Filipe Leoncio <filipealeoncio@gmail.com>
2022-08-06 00:29:40 -0300

    refactor: Atualização dos mocks de chamada de API

--- src/services/paredao.js
+++ src/services/paredao.js
@@ -7,25 +7,25 @@ export default class ParedaoService {
     }
 
     getParedaoVigenteInfo = () => {
-        const mockParedaoInfo = {
+        const mockResParedaoVigenteInfo = {
             infoParticipantes: [
                 { img: images.participante1, nome: 'Participante 1', sexo: 'f' },
                 { img: images.participante2, nome: 'Participante 2', sexo: 'm' },
             ],
         };
-        return mockParedaoInfo;
+        return mockResParedaoVigenteInfo;
 
         //solução final
         // return this.api.get(ENDPOINT, payload);
     };
 
     postVoto = (payload) => {
-        const mockParedaoInfo = {
+        const mockResPostVoto = {
             porcentagemVotosParticipante1: 65,
             diaHoraInicio: '01/08/2022-20:00:00',
-            diaHoraFim: '07/08/2022-12:00:00',
+            diaHoraFim: '09/08/2022-20:00:00',
         };
-        return mockParedaoInfo;
+        return mockResPostVoto;
 
         //solução final
         // return this.api.post(ENDPOINT, payload);
