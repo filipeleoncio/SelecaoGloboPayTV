417dde08f2b166ffba4361bedd261bc07106dae5
Filipe Leoncio <filipealeoncio@gmail.com>
2022-08-05 21:55:02 -0300

    feat: Adicionado novas constantes de votação

--- src/constants/votacao.js
+++ src/constants/votacao.js
@@ -1,22 +1,29 @@
 export const VOTACAO_TEXT = {
-    titulo: (
+    TITULO: (
         <>
             QUEM DEVE SER <b>ELIMINADO</b>?
         </>
     ),
-    descricaoParticipante1: (nome, sexo) => (
+    DESCRICAO_PARTICIPANTE_1: (nome, sexo) => (
         <>
             Para eliminar {sexo === 'm' ? 'o' : 'a'} <b>{nome}</b> pelo telefone disque <b>0800-123-001</b> ou mande um
             SMS para <b>8001</b>
         </>
     ),
-    descricaoParticipante2: (nome, sexo) => (
+    DESCRICAO_PARTICIPANTE_2: (nome, sexo) => (
         <>
             Para eliminar {sexo === 'm' ? 'o' : 'a'} <b>{nome}</b> pelo telefone disque <b>0800-123-002</b> ou mande um
             SMS para <b>8002</b>
         </>
     ),
-    buttonConfirmarText: 'Envie seu voto agora',
+    BUTTON_CONFIRMAR: 'Envie seu voto agora',
+    RESULTADO_SUBTITULO: (nome) => (
+        <p>
+            <b>Parabéns!</b> Seu voto para <b>{nome}</b> foi enviado com sucesso.
+        </p>
+    ),
+    RESULTADO_PARTE_1: 'FALTAM',
+    RESULTADO_PARTE_2: 'PARA ENCERRAR A VOTAÇÃO',
 };
 
 export const VOTACAO_STEPS = {
